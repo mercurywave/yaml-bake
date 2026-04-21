46dc775de689ea7d0c590a6f4e3a1b33446a0a69
mercurywave <alex.nelson@mercury-wave.com>
2026-04-20 23:12:42 -0500

    try to fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
     clean: true,
-    publicPath: '/'
+    publicPath: './'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
